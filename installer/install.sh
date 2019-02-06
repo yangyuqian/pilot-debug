5db71bd4d8e53d3d79717b2c0ae05c2c9b7a1932
Yuqian Yang <yyang@freewheel.tv>
2019-02-06 11:40:54 +0800

    upgrade pilot-debug

--- installer/install.sh
+++ installer/install.sh
@@ -12,19 +12,25 @@ function reinstall(){
   if [ -n "$2" ]; then
     ns=$2
   fi
+
+  echo "initialize namespace $ns ..."
+  wt
+  kubectl create ns $ns
+
   echo "cleanup $1 ..."
+  wt
   kubectl delete --namespace $ns -f $1 || echo "removed $1 ..."
 
-  wt
   echo "installing $1 ..."
+  wt
   kubectl apply --namespace $ns -f $1
 }
 
-# reinstall "istio-full.yaml" "istio-system"
-reinstall "istio-no-crd.yaml" "istio-system"
-# reinstall "samples/networking/soa-gateway.yaml"
+reinstall "istio-full.yaml" "istio-system"
+# reinstall "istio-no-crd.yaml" "istio-system"
+reinstall "samples/networking/soa-gateway.yaml"
 # reinstall "samples/networking/virtualservice-destinationrule-solr.yaml"
-# reinstall "samples/networking/virtualservice-destinationrule-grafana.yaml"
-# reinstall "samples/networking/virtualservice-destinationrule-servicegraph.yaml"
-# reinstall "samples/networking/virtualservice-destinationrule-tracing.yaml"
+reinstall "samples/networking/virtualservice-destinationrule-grafana.yaml"
+reinstall "samples/networking/virtualservice-destinationrule-servicegraph.yaml"
+reinstall "samples/networking/virtualservice-destinationrule-tracing.yaml"
 # reinstall "samples/networking/virtualservice-destinationrule-pilot.yaml"
